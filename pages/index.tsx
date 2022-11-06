9a6ca8f20132a04e0ed6fcffd78bbda3a24e9578
Ryo <ryogassk424@gmail.com>
2022-11-06 21:14:25 +0900

    Todoリストを作ってTypescriptを学びました

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,83 @@
-import { NextPage } from 'next'
+import type { NextPage } from "next";
+import { ChangeEventHandler, MouseEventHandler, useState, VFC } from "react";
 
+type Todo = {
+  id: number;
+  label: string;
+  isDone: boolean;
+};
 
 const Home: NextPage = () => {
+  const [text, setText] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const input: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setText(e.target.value);
+  };
+
+  const add: MouseEventHandler<HTMLButtonElement> = () => {
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: Math.random(), label: text, isDone: false }];
+    });
+    setText("");
+  };
+
+  const toggle: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === Number(e.target.value)) {
+          return { ...todo, isDone: !todo.isDone };
+        }
+        return todo;
+      });
+    });
+  };
+
   return (
- <div>test</div>
-  )
-}
+    <div className="w-96 mx-auto p-20">
+      <h1 className="text-xl font-bold">Todo</h1>
+      <div className="flex gap-x-2">
+        <input
+          type="text"
+          value={text}
+          onChange={input}
+          className="border border-black"
+        />
+        <button
+          onClick={add}
+          className="border border-black flex-shrink-0 px-2"
+        >
+          追加
+        </button>
+      </div>
+      <ul className="mt-4 space-y-2">
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <ListItem todo={todo} toggle={toggle} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+type ListItemProps = {
+  todo: Todo;
+  toggle: ChangeEventHandler<HTMLInputElement>;
+};
+
+const ListItem: VFC<ListItemProps> = ({ todo, toggle }) => {
+  return (
+    <label className="flex items-center gap-x-2">
+      <input
+        type="checkbox"
+        value={todo.id}
+        checked={todo.isDone}
+        onChange={toggle}
+      />
+      <span>{todo.label}</span>
+    </label>
+  );
+};
 
 export default Home;
